cebf3c3cbdd541523895d203fe7018ea7e9bc23f
Raymond Thurman <raymondthurman5@gmail.com>
2020-08-25 18:50:16 -0400

    ✨ added a graph and stack to ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,38 @@
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            # because sets are faster and they are hashtables underneath the hood
+            self.vertices[vertex_id] = set()
+
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+
+    def get_neighbors(self, vertex_id):
+        return self.vertices[vertex_id]
+
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
+
 
 def earliest_ancestor(ancestors, starting_node):
     pass
